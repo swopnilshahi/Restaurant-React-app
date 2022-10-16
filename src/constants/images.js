c6b9c52a5e0aa4ccc473d6bc970d1410bece95bd
Swopnil <swopnil@divyaltech.com>
2022-10-16 07:22:41 +0545

    find Us

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,18 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
+import logo512 from '../assets/logo512.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
+import gallery05 from '../assets/gallery05.jpg';
+import gallery06 from '../assets/gallery06.jpg';
+import gallery07 from '../assets/gallery07.jpg';
+import gallery08 from '../assets/gallery08.jpg';
+
+
+
 import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.png';
@@ -25,10 +33,16 @@ export default {
   bg,
   chef,
   G,
+  logo512,
   gallery01,
   gallery02,
   gallery03,
   gallery04,
+  gallery05,
+  gallery06,
+  gallery07,
+  gallery08,
+  
   knife,
   logo,
   menu,
