83a14f989f9d2852bffababd33f5a6f76ffc4f81
Swopnil <swopnil@divyaltech.com>
2022-10-17 05:32:29 +0545

    fixed issue and Footer Added

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -22,7 +22,7 @@ const Chef = () => (
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
+        <p>Krishna lmo</p>
         <p className="p__opensans">Chef & Founder</p>
         <img src={images.sign} alt="sign_image" />
       </div>
