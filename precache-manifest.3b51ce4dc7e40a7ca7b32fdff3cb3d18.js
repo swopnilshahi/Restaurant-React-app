8334218d7c59839a7e34f885b25016aa7a99dcab
Swopnil <swopnil@divyaltech.com>
2022-10-17 06:39:10 +0545

    Updates

---
+++ precache-manifest.3b51ce4dc7e40a7ca7b32fdff3cb3d18.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8d3236595ef0c1dd158",
+    "url": "/Restaurant-React-app/static/css/main.5c8c42a4.chunk.css"
+  },
+  {
+    "revision": "d8d3236595ef0c1dd158",
+    "url": "/Restaurant-React-app/static/js/main.d8d32365.chunk.js"
+  },
+  {
+    "revision": "89453521fde264b603a0",
+    "url": "/Restaurant-React-app/static/js/1.89453521.chunk.js"
+  },
+  {
+    "revision": "c09dce7e44e6245d0cf7",
+    "url": "/Restaurant-React-app/static/js/runtime~main.c09dce7e.js"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/Restaurant-React-app/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/Restaurant-React-app/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/Restaurant-React-app/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/Restaurant-React-app/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/Restaurant-React-app/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/Restaurant-React-app/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/Restaurant-React-app/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/Restaurant-React-app/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "49837c0c863a5df0d177807512382b90",
+    "url": "/Restaurant-React-app/static/media/gallery05.49837c0c.jpg"
+  },
+  {
+    "revision": "0acb534b4767eccaae9c06f167e4f706",
+    "url": "/Restaurant-React-app/static/media/gallery06.0acb534b.jpg"
+  },
+  {
+    "revision": "339b67543ccb162bccbe04289d5235c2",
+    "url": "/Restaurant-React-app/static/media/gallery07.339b6754.jpg"
+  },
+  {
+    "revision": "144e3856a0b54bbe5e7e99b2d26995a2",
+    "url": "/Restaurant-React-app/static/media/gallery08.144e3856.jpg"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/Restaurant-React-app/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/Restaurant-React-app/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "e9a9946252d92da7c7887efc808e66ca",
+    "url": "/Restaurant-React-app/static/media/menu.e9a99462.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/Restaurant-React-app/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/Restaurant-React-app/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "fd16528f71a845cc74618cf2bd3d8995",
+    "url": "/Restaurant-React-app/static/media/welcome.fd16528f.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/Restaurant-React-app/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/Restaurant-React-app/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/Restaurant-React-app/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/Restaurant-React-app/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/Restaurant-React-app/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/Restaurant-React-app/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/Restaurant-React-app/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "f95cf3471139e4352181f8796ea648ae",
+    "url": "/Restaurant-React-app/index.html"
+  }
+];
